6e2ddead5255fbf8920b0b84e3863db8a4d267e4
Margaret Clinard <margaret.clinard@gmail.com>
2015-06-15 13:25:57 -0500

    Basic user sign up/in/out.

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  authenticates_with_sorcery!
+
+  validates :email, :name, presence: true
+  validates :name, length: { minimum: 3 }
+  validates :email, format: { with: /.+@.+\..+/, message: "must be an email address" }, uniqueness: true
+  validates :password, confirmation: true
+  validates :password, :password_confirmation, presence: { on: :create }
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+RSpec.describe User, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(3) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_confirmation_of(:password) }
+    it { should validate_presence_of(:password).on(:create) }
+    it { should_not validate_presence_of(:password).on(:update) }
+    it { should validate_presence_of(:password_confirmation).on(:create) }
+    it { should_not validate_presence_of(:password_confirmation).on(:update) }
+    it "should allow valid values for email" do
+      should allow_value("eliza@elizamarcum.com", "a@b.co.uk", "eliza+hash@example.com").for(:email)
+    end
+    describe "should be invalid if email is not formatted correctly" do
+      it { should_not allow_value("elizabrock.com").for(:email) }
+      it { should_not allow_value("eliza@examplecom").for(:email) }
+      it { should_not allow_value("@.com").for(:email) }
+    end
+    it "should have a working factory" do
+      Fabricate.build(:user).should be_valid
+    end
+  end
+end
